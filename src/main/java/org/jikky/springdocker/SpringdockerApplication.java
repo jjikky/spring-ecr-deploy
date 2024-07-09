7741d1a47781d36fcf864b1b365cee1c481edcf8
jjikky <kok5417@skuniv.ac.kr>
2024-07-09 20:47:10 +0900

    first commit

---
+++ src/main/java/org/jikky/springdocker/SpringdockerApplication.java
@@ -0,0 +1,23 @@
+package org.jikky.springdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringdockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdockerApplication.class, args);
+    }
+}
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "지키의 스프링 서버";
+    }
+}
---
+++ src/test/java/org/jikky/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jikky.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
